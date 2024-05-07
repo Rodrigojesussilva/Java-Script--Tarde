9ce8bceef4098818b4aece4bf8900717a1ec9cfe
Rodrigo <rodrigo.jesussilva@hotmail.com>
2024-05-07 13:53:24 -0300

    update

---
+++ 001-variavel.js
@@ -0,0 +1,34 @@
+
+
+/*
+*****
+VAR
+*****
+var nome = "Rodrigo"
+if (true) 
+{
+   var nome = "Jesus"
+}
+var nome = "Silva"
+console.log(nome)
+*/
+
+//LET variavel de escopo
+
+/*
+if (true) 
+{
+    let nome = "Jesus"
+   
+}
+
+console.log(nome)
+*/
+
+//CONST
+
+
+const nome = "Jesus"     
+    
+ 
+console.log(nome)
---
+++ 005-operadores_aritimeticos.js
@@ -0,0 +1,41 @@
+/*
+    Adição (+)
+    Subtração (-)
+    Divisão (/)
+    Multiplicação (*)
+    Resto da divisão (%)
+    Incremento(++)
+    Descremento(--)
+    Expoente(**)
+*/
+//operador aritimetico SOMA
+let v1 = 2
+let v2 = 3
+let soma = v1+v2
+console.log("A soma e:"+soma)
+console.log("A soma e:",v1+v2)//Conversão
+console.log("A soma e:"+Number(v1+v2))//Conversão
+
+//operador aritimetico SUBTRAÇÃO
+let v3 = 10
+let v4 = 5
+let subtração = v3-v4
+console.log("A subtração e:"+subtração)
+console.log("A subtração e:",v3-v4)//Conversão
+console.log("A subtração e:"+Number(v3-v4))//Conversão
+
+//operador aritimetico DIVISÃO
+let v5 = 10
+let v6 = 5
+let divisao = v5/v6
+console.log("A divisão e:"+divisao)
+console.log("A divisão e:",v5/v6)//Conversão
+console.log("A divisão e:"+Number(v5/v6))//Conversão
+
+//operador aritimetico multiplicação
+let v7 = 10
+let v8 = 5
+let multi = v7*v8
+console.log("A multiplicação e:"+multi)
+console.log("A multiplicação e:",v7*v8)//Conversão
+console.log("A multiplicação e:"+Number(v7*v8))//Conversão

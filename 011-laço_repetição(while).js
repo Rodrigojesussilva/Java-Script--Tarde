cf56431d2f49d3d44e7d64cb92df843e796636a6
Rodrigo <rodrigo.jesussilva@hotmail.com>
2024-05-13 14:22:55 -0300

    Update

--- 011-laço_repetição(while).js
+++ 011-laço_repetição(while).js
@@ -8,11 +8,38 @@
     específica seja atendida.
 */
 
-//contagem regressiva usando while
-let contador = 5;
-while (contador >= 1) {
-  console.log(contador);
-  contador--;
+//while
+let i = 0;
+while (i <= 5) {
+    console.log(i);
+    i++;
+}
+
+const frutas = ['maça', 'banana', 'laranja', 'pera', 'melancia'];
+let j = 0;
+while (j < frutas.length) {
+    console.log('Eu gosto de ' + frutas[j]);
+    j++;
+}
+
+const person = {
+    firstName: 'John',
+    lastName: 'Doe',
+    age: 30
+};
+
+let key = 0;
+while(key in person) {
+    console.log(key + ': ' + person[key]);
+}
+
+let k = 0;
+while (k < 10) {
+    if (k === 5) {
+        break;
+    }
+    console.log(k);
+    k++;
 }
 
 
